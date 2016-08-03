54a93c6de909ab4b9a82b9546cff072701633191
Pierre-Henri Trivier <phtrivier@sierrawireless.com>
2016-08-03 14:25:54 +0200

    Personnal modifications

---
+++ personal/custom.el
@@ -0,0 +1,19 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(global-linum-mode t)
+ '(global-whitespace-mode nil)
+ '(linum-format "%3d")
+ '(org-agenda-files (quote ("~/pro.org")))
+ '(org-log-done nil)
+ '(whitespace-global-modes nil)
+ '(whitespace-line-column 256))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(linum ((t (:background "black" :foreground "#9FC59F")))))
