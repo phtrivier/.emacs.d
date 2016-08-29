9986ab96f528392fb097ee9d3203eaf093d681d5
Pierre-Henri Trivier <phtrivier@sierrawireless.com>
2016-08-29 18:30:18 +0200

    Added org colors

--- personal/custom.el
+++ personal/custom.el
@@ -5,9 +5,14 @@
  ;; If there is more than one, they won't work right.
  '(global-linum-mode t)
  '(global-whitespace-mode nil)
+ '(helm-ag-insert-at-point (quote word))
+ '(helm-buffer-max-length 80)
  '(linum-format "%3d")
- '(org-agenda-files (quote ("~/pro.org")))
+ '(org-agenda-files (quote ("~/org/pro.org")) (quote ("~/org/perso.org")))
  '(org-log-done nil)
+ '(org-todo-keyword-faces (quote (("WAIT" . "#ff8c00") ("REVIEW" . "#1e90ff"))))
+ '(paradox-github-token t)
+ '(prelude-guru nil)
  '(whitespace-global-modes nil)
  '(whitespace-line-column 256))
 (custom-set-faces
@@ -16,4 +21,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:inherit nil :stipple nil :background "black" :foreground "#DCDCCC" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 128 :width normal :foundry "DAMA" :family "Ubuntu Mono"))))
+ '(flycheck-color-mode-line-error-face ((t (:inherit flycheck-fringe-error :background "dark red" :foreground "#efefef" :weight normal))))
  '(linum ((t (:background "black" :foreground "#9FC59F")))))
--- personal/pht.el
+++ personal/pht.el
@@ -1,1 +1,13 @@
 (global-set-key (kbd "C-x C-r") 'recentf-open-files)
+
+(global-set-key (kbd "C-c h") 'helm-projectile-find-file)
+
+(global-set-key (kbd "C-c C-b") 'recompile)
+
+(add-to-list 'auto-mode-alist '("\\.restclient\\'" . restclient-mode))
+(add-to-list 'auto-mode-alist '("\\.cf\\'" . json-mode))
+
+(require 'flycheck-color-mode-line)
+
+(eval-after-load "flycheck"
+  '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
